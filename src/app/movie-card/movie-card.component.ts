702c730487b1786fb4c0b55819f04f24f9fd10d9
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2022-04-01 20:12:08 +0200

    Update movie-card.component.ts
    Adding Movie-view, Director-view, Genre-view and Logout functions

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { MovieViewComponent } from '../movie-view/movie-view.component';
+import { GenreViewComponent } from '../genre-view/genre-view.component';
+import { DirectorViewComponent } from '../director-view/director-view.component';
 
 @Component({
   selector: 'app-movie-card',
@@ -11,7 +17,12 @@ export class MovieCardComponent {
 
   movies: any[] = [];
 
-  constructor(public fetchApiData: FetchApiDataService) { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog,
+    public router: Router,
+    public snackBar: MatSnackBar,
+  ) { }
 
   ngOnInit(): void {
     this.getMovies();
@@ -23,5 +34,45 @@ export class MovieCardComponent {
       console.log(this.movies);
       return this.movies;
     });
+  }
+
+  openMovieDialog(title: string, poster: any, description: string): void {
+    this.dialog.open(MovieViewComponent, {
+      data: {
+        Title: title,
+        Poster: poster,
+        Description: description,
+      },
+      width: '500px',
+    });
+  }
+
+  openGenreDialog(name: string, description: string): void {
+    this.dialog.open(GenreViewComponent, {
+      data: {
+        name,
+        description,
+      },
+      width: '500px',
+    });
+  }
+
+  openDirectorDialog(name: string, bio: string, birthdate: Date): void {
+    this.dialog.open(DirectorViewComponent, {
+      data: { name, bio, birthdate },
+      width: '500px',
+    });
+  }
+
+  logOut(): void {
+    localStorage.clear();
+    this.snackBar.open('You have been successfully logged out', 'Ok', {
+      duration: 2000,
+    });
+    this.router.navigate(['welcome']);
+  }
+
+  goToMoviesPage(): void {
+    this.router.navigate(['movies']);
   }
 }
