e7d7244396726b34c191c9b95d3409ab7e4c8591
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2022-04-02 01:49:31 +0200

    Create user-edit.component.ts
    Adding User-view and User-edit components

---
+++ src/app/user-edit/user-edit.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-user-edit',
+  templateUrl: './user-edit.component.html',
+  styleUrls: ['./user-edit.component.css']
+})
+export class UserEditComponent implements OnInit {
+  @Input() userData = { Username: '', Password: '', EmailID: '', Birth: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserEditComponent>,
+    public snackBar: MatSnackBar,
+    public router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  editUserProfile(): void {
+    this.fetchApiData.editUserProfile(this.userData).subscribe((res) => {
+      this.dialogRef.close();
+      window.location.reload();
+      localStorage.setItem('username', res.Username)
+      console.log(res)
+      this.snackBar.open(this.userData.Username, 'Successfully updated user details!', {
+        duration: 3000
+      });
+    }, (res) => {
+      this.snackBar.open(res, 'OK', {
+        duration: 3000
+      });
+    })
+  }
+
+}
